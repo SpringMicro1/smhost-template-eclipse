8078d4a44b58ac51389343848e64cdff3a28db3b
David Buckley <runalldayandnight@gmail.com>
2023-06-07 22:29:58 -0600

    rename all to eclipse

--- src/config.ts
+++ src/config.ts
@@ -1,155 +1,180 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const BASE = {
-  TITLE: "Astrolus",
-  COMPANY_NAME: "Astrolus",
-  DESC: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  LINK_FIXED_HREF: "https://www.example.com",
-  LINK_FIXED_CONTENT: "Get Started Now",
-  LINK_GITHUB: "https://www.github.com/account/repository",
-  LINK_TWITTER: "https://www.twitter.com/account",
-  LINK_YOUTUBE: "https://www.youtube.com/@account",
-  LINK_FACEBOOK: "https://www.facebook.com/account",
-  LINK_MEDIUM: "https://www.medium.com/blog",
-  LINK_PINTEREST: "https://www.pinterest.com/account",
-  LINK_PATREON: "https://www.patreon.com/account",
-  LINK_INSTAGRAM: "https://www.instagram.com/account",
-  MISSION_STATEMENT: "We change the way UI components libraries are used",
-  COPYRIGHT_OWNER: "Tailus Blocks",
-};
-export const HOME = {
-  TEXT: {
-    HEADING: "Shaping a world with reimagination.",
-    DESCRIPTION: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt nam itaque sed eius modi error totam sit illum. Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis beatae ipsum soluta!",
-    BLURB_1_TITLE: "The lowest price",
-    BLURB_1_TEXT: "Some text here",
-    BLURB_2_TITLE: "The fastest on the market",
-    BLURB_2_TEXT: "Some text here",
-    BLURB_3_TITLE: "The most loved",
-    BLURB_3_TEXT: "Some text here",
-    SECTION_1_TITLE: "A technology-first approach to payments and finance",
-    SECTION_1_TEXT: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ad ipsum pariatur autem, fugit laborum in atque amet obcaecati? Nisi minima aspernatur, quidem nulla cupiditate nam consequatur eligendi magni adipisci.",
-    FEATURE_1_TITLE: "First feature",
-    FEATURE_1_TEXT: "Neque Dolor, fugiat non cum doloribus aperiam voluptates nostrum.",
-    FEATURE_2_TITLE: "Second feature",
-    FEATURE_2_TEXT: "Neque Dolor, fugiat non cum doloribus aperiam voluptates nostrum.",
-    FEATURE_3_TITLE: "Third feature",
-    FEATURE_3_TEXT: "Neque Dolor, fugiat non cum doloribus aperiam voluptates nostrum.",
-    FEATURE_4_TITLE: "More features",
-    FEATURE_4_TEXT: "Neque Dolor, fugiat non cum doloribus aperiam voluptates nostrum.",
-    SECTION_2_TITLE: "Nuxt development is carried out by passionate developers", 
-    SECTION_2_TEXT_P1:"Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.",
-    SECTION_2_TEXT_P2:"Vitae error, quaerat officia delectus voluptatibus explicabo quo pariatur impedit, at reprehenderit aliquam a ipsum quas voluptatem.Quo pariatur asperiores eum amet.",
-    HIGHLIGHT_1_TITLE: "Chat Anytime",
-    HIGHLIGHT_1_TEXT: "Asperiores nemo possimus nesciunt quam mollitia.",
-    HIGHLIGHT_2_TITLE: "Real Time Location",
-    HIGHLIGHT_2_TEXT: "Asperiores nemo possimus nesciunt quam mollitia.",
-    TESTIMONIALS_HEADING: "We have some fans.",
-    TESTIMONIAL_1_NAME: "Daniella Doe",
-    TESTIMONIAL_1_SUBTITLE: "Mobile dev",
-    TESTIMONIAL_1_TEXT: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aliquid quo eum quae quos illo earum ipsa doloribus nostrum minus libero aspernatur laborum cum, a suscipit, ratione ea totam ullam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
-    TESTIMONIAL_2_NAME: "Yanick Doe",
-    TESTIMONIAL_2_SUBTITLE: "Developer",
-    TESTIMONIAL_2_TEXT: "Lorem ipsum dolor laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
-    TESTIMONIAL_3_NAME: "Andy Doe",
-    TESTIMONIAL_3_SUBTITLE: "Manager",
-    TESTIMONIAL_3_TEXT: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
-    TESTIMONIAL_4_NAME: "Jane doe",
-    TESTIMONIAL_4_SUBTITLE: "Marketing",
-    TESTIMONIAL_4_TEXT: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
-    TESTIMONIAL_5_NAME: "Jane Doe",
-    TESTIMONIAL_5_SUBTITLE: "Mobile dev",
-    TESTIMONIAL_5_TEXT: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
-    TESTIMONIAL_6_NAME: "Yanndy Doe",
-    TESTIMONIAL_6_SUBTITLE: "Mobile dev",
-    TESTIMONIAL_6_TEXT: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
-    CALL_TO_ACTION_TITLE: "Get Started now",
-    CALL_TO_ACTION_TEXT: "Be part of millions people around the world using tailus in modern User Interfaces.",
-    BLOG_TITLE: "Latest Articles",
-    CONTACT_TAGLINE: "Get in touch.",
-    CONTACT_PARAGRAPH: "Have something to say? We are here to help. Fill out the form, send an email, or call us on the phone.",
-    ADDRESS: "123 Example Street; My Place, NY 00000",
-    BLOG_TEXT: "Quam hic dolore cumque voluptate rerum beatae et quae, tempore sunt, debitis dolorum officia aliquid explicabo? Excepturi, voluptate?",
+const config = {
+  BASE: {
+    OPENGRAPH_IMAGE_ALT_TEXT: "Screenshot of Eclipse Template",
+    OPENGRAPH_TITLE: "Eclipse",
+    META_TITLE_DEFAULT: "Eclipse",
+    COMPANY_NAME: "Eclipse",
+    META_DESCRIPTION:
+      "A minimal, responsive and SEO-friendly Astro blog theme.",
+    LINK_FIXED_HREF: "https://www.example.com",
+    LINK_FIXED_CONTENT: "Get Started Now",
+    LINK_GITHUB: "https://www.github.com/account/repository",
+    LINK_TWITTER: "https://www.twitter.com/account",
+    LINK_YOUTUBE: "https://www.youtube.com/@account",
+    LINK_FACEBOOK: "https://www.facebook.com/account",
+    LINK_MEDIUM: "https://www.medium.com/blog",
+    LINK_PINTEREST: "https://www.pinterest.com/account",
+    LINK_PATREON: "https://www.patreon.com/account",
+    LINK_INSTAGRAM: "https://www.instagram.com/account",
+    MISSION_STATEMENT: "We change the way UI components libraries are used",
+    COPYRIGHT_OWNER: "SpringMicroHost",
   },
-  LINKS: {
-    LINK_PRIMARY: { 
-      href: "https://www.example.com",
-      content: "Get started"
-    }, 
-    LINK_SECONDARY: { 
-      href: "https://www.example.com",
-      content: "Learn more"
+  HOME: {
+    TEXT: {
+      HEADING: "Shaping a world with reimagination.",
+      DESCRIPTION:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt nam itaque sed eius modi error totam sit illum. Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis beatae ipsum soluta!",
+      BLURB_1_TITLE: "The lowest price",
+      BLURB_1_TEXT: "Some text here",
+      BLURB_2_TITLE: "The fastest on the market",
+      BLURB_2_TEXT: "Some text here",
+      BLURB_3_TITLE: "The most loved",
+      BLURB_3_TEXT: "Some text here",
+      SECTION_1_TITLE: "A technology-first approach to payments and finance",
+      SECTION_1_TEXT:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ad ipsum pariatur autem, fugit laborum in atque amet obcaecati? Nisi minima aspernatur, quidem nulla cupiditate nam consequatur eligendi magni adipisci.",
+      FEATURE_1_TITLE: "First feature",
+      FEATURE_1_TEXT:
+        "Neque Dolor, fugiat non cum doloribus aperiam voluptates nostrum.",
+      FEATURE_2_TITLE: "Second feature",
+      FEATURE_2_TEXT:
+        "Neque Dolor, fugiat non cum doloribus aperiam voluptates nostrum.",
+      FEATURE_3_TITLE: "Third feature",
+      FEATURE_3_TEXT:
+        "Neque Dolor, fugiat non cum doloribus aperiam voluptates nostrum.",
+      FEATURE_4_TITLE: "More features",
+      FEATURE_4_TEXT:
+        "Neque Dolor, fugiat non cum doloribus aperiam voluptates nostrum.",
+      SECTION_2_TITLE:
+        "Nuxt development is carried out by passionate developers",
+      SECTION_2_TEXT_P1:
+        "Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at? Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.",
+      SECTION_2_TEXT_P2:
+        "Vitae error, quaerat officia delectus voluptatibus explicabo quo pariatur impedit, at reprehenderit aliquam a ipsum quas voluptatem.Quo pariatur asperiores eum amet.",
+      HIGHLIGHT_1_TITLE: "Chat Anytime",
+      HIGHLIGHT_1_TEXT: "Asperiores nemo possimus nesciunt quam mollitia.",
+      HIGHLIGHT_2_TITLE: "Real Time Location",
+      HIGHLIGHT_2_TEXT: "Asperiores nemo possimus nesciunt quam mollitia.",
+      TESTIMONIALS_HEADING: "We have some fans.",
+      TESTIMONIAL_1_NAME: "Daniella Doe",
+      TESTIMONIAL_1_SUBTITLE: "Mobile dev",
+      TESTIMONIAL_1_TEXT:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aliquid quo eum quae quos illo earum ipsa doloribus nostrum minus libero aspernatur laborum cum, a suscipit, ratione ea totam ullam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
+      TESTIMONIAL_2_NAME: "Yanick Doe",
+      TESTIMONIAL_2_SUBTITLE: "Developer",
+      TESTIMONIAL_2_TEXT:
+        "Lorem ipsum dolor laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
+      TESTIMONIAL_3_NAME: "Andy Doe",
+      TESTIMONIAL_3_SUBTITLE: "Manager",
+      TESTIMONIAL_3_TEXT:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
+      TESTIMONIAL_4_NAME: "Jane doe",
+      TESTIMONIAL_4_SUBTITLE: "Marketing",
+      TESTIMONIAL_4_TEXT:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
+      TESTIMONIAL_5_NAME: "Jane Doe",
+      TESTIMONIAL_5_SUBTITLE: "Mobile dev",
+      TESTIMONIAL_5_TEXT:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
+      TESTIMONIAL_6_NAME: "Yanndy Doe",
+      TESTIMONIAL_6_SUBTITLE: "Mobile dev",
+      TESTIMONIAL_6_TEXT:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.",
+      CALL_TO_ACTION_TITLE: "Get Started now",
+      CALL_TO_ACTION_TEXT:
+        "Be part of millions people around the world using tailus in modern User Interfaces.",
+      BLOG_TITLE: "Latest Articles",
+      CONTACT_TAGLINE: "Get in touch.",
+      CONTACT_PARAGRAPH:
+        "Have something to say? We are here to help. Fill out the form, send an email, or call us on the phone.",
+      ADDRESS: "123 Example Street; My Place, NY 00000",
+      BLOG_TEXT:
+        "Quam hic dolore cumque voluptate rerum beatae et quae, tempore sunt, debitis dolorum officia aliquid explicabo? Excepturi, voluptate?",
     },
-    LINK_FIXED: { 
-      href: "https://www.example.com",
-      content: ""
+    LINKS: {
+      LINK_PRIMARY: {
+        href: "https://www.example.com",
+        content: "Get started",
+      },
+      LINK_SECONDARY: {
+        href: "https://www.example.com",
+        content: "Learn more",
+      },
+      LINK_FIXED: {
+        href: "https://www.example.com",
+        content: "",
+      },
+      CALL_TO_ACTION_LINK_PRIMARY: {
+        href: "https://www.example.com",
+        content: "Get Started",
+      },
+      CALL_TO_ACTION_LINK_SECONDARY: {
+        href: "https://www.example.com",
+        content: "More about",
+      },
+      EMAIL: {
+        href: "me@example.com",
+        content: "me@example.com",
+      },
+      PHONE: {
+        href: "+12015555555",
+        content: "+1 (201) 555-5555",
+      },
     },
-    CALL_TO_ACTION_LINK_PRIMARY: { 
-      href: "https://www.example.com",
-      content: "Get Started"
-    }, 
-    CALL_TO_ACTION_LINK_SECONDARY: { 
-      href: "https://www.example.com",
-      content: "More about"
-    },
-    EMAIL: {
-      href: "me@example.com",
-      content: "me@example.com",
-    },
-    PHONE: {
-      href: "+12015555555",
-      content: "+1 (201) 555-5555",
+    IMAGES: {
+      CLIENT_1: {
+        src: "https://astrolus.netlify.app/images/clients/microsoft.svg",
+        alt: "microsoft",
+      },
+      CLIENT_2: {
+        src: "https://astrolus.netlify.app/images/clients/airbnb.svg",
+        alt: "airbnb",
+      },
+      CLIENT_3: {
+        src: "https://astrolus.netlify.app/images/clients/google.svg",
+        alt: "google",
+      },
+      CLIENT_4: {
+        src: "https://astrolus.netlify.app/images/clients/ge.svg",
+        alt: "ge",
+      },
+      CLIENT_5: {
+        src: "https://astrolus.netlify.app/images/clients/netflix.svg",
+        alt: "netflix",
+      },
+      CLIENT_6: {
+        src: "https://astrolus.netlify.app/images/clients/google-cloud.svg",
+        alt: "google cloud",
+      },
+      TESTIMONIAL_1_AVATAR: {
+        src: "https://astrolus.netlify.app/images/avatars/avatar.webp",
+        alt: "user avatar",
+      },
+      TESTIMONIAL_2_AVATAR: {
+        src: "https://astrolus.netlify.app/images/avatars/avatar-1.webp",
+        alt: "user avatar",
+      },
+      TESTIMONIAL_3_AVATAR: {
+        src: "https://astrolus.netlify.app/images/avatars/avatar-2.webp",
+        alt: "user avatar",
+      },
+      TESTIMONIAL_4_AVATAR: {
+        src: "https://astrolus.netlify.app/images/avatars/avatar-3.webp",
+        alt: "user avatar",
+      },
+      TESTIMONIAL_5_AVATAR: {
+        src: "https://astrolus.netlify.app/images/avatars/avatar-4.webp",
+        alt: "user avatar",
+      },
+      TESTIMONIAL_6_AVATAR: {
+        src: "https://astrolus.netlify.app/images/avatars/avatar-2.webp",
+        alt: "user avatar",
+      },
     },
   },
-  IMAGES: {
-    CLIENT_1: {
-      src: "https://astrolus.netlify.app/images/clients/microsoft.svg",
-      alt: "microsoft"
-    }, 
-    CLIENT_2: {
-      src: "https://astrolus.netlify.app/images/clients/airbnb.svg",
-      alt: "airbnb"
-    }, 
-    CLIENT_3: {
-      src: "https://astrolus.netlify.app/images/clients/google.svg",
-      alt: "google"
-    }, 
-    CLIENT_4: {
-      src: "https://astrolus.netlify.app/images/clients/ge.svg",
-      alt: "ge"
-    }, 
-    CLIENT_5: {
-      src: "https://astrolus.netlify.app/images/clients/netflix.svg",
-      alt: "netflix"
-    }, 
-    CLIENT_6: {
-      src: "https://astrolus.netlify.app/images/clients/google-cloud.svg",
-      alt: "google cloud"
-    }, 
-    TESTIMONIAL_1_AVATAR: {
-      src: "https://astrolus.netlify.app/images/avatars/avatar.webp",
-      alt: "user avatar"
-    }, 
-    TESTIMONIAL_2_AVATAR: {
-      src: "https://astrolus.netlify.app/images/avatars/avatar-1.webp",
-      alt: "user avatar"
-    }, 
-    TESTIMONIAL_3_AVATAR: {
-      src: "https://astrolus.netlify.app/images/avatars/avatar-2.webp",
-      alt: "user avatar"
-    }, 
-    TESTIMONIAL_4_AVATAR: {
-      src: "https://astrolus.netlify.app/images/avatars/avatar-3.webp",
-      alt: "user avatar"
-    }, 
-    TESTIMONIAL_5_AVATAR: {
-      src: "https://astrolus.netlify.app/images/avatars/avatar-4.webp",
-      alt: "user avatar"
-    }, 
-    TESTIMONIAL_6_AVATAR: {
-      src: "https://astrolus.netlify.app/images/avatars/avatar-2.webp",
-      alt: "user avatar"
-    }
-  }
 };
+export const BASE = config.BASE;
+export const HOME = config.HOME;
---
+++ src/content/team/firstname-lastname.md
@@ -0,0 +1,11 @@
+---
+draft: false
+name: "FirstName LastName"
+title: "Blog Author"
+avatar:
+  {
+    src: "https://images.unsplash.com/photo-1580489944761-15a19d654956?&fit=crop&w=280",
+    alt: "Janette Lynch",
+  }
+publishDate: "2023-01-01 12:00"
+---
